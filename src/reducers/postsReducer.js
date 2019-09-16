2ec3e7519af03396bed98387d931ffe10e9a06a8
jeff.smith <slightlyoffbass@gmail.com>
2019-09-16 14:10:42 -0500

    added javascript notes on reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,4 +1,4 @@
-export default  () =>{
+export default  (state, action) =>{
     return 123;
 };
 
@@ -8,4 +8,32 @@ export default  () =>{
 // without a return statement, undefined will be default
 // RULE 2: Pruces 'state', or data to be used inside of your app using only preivious stae and the action
 // RULE 3: Must not return reach 'out of itself' to decide what value to return (reducers are pure)
-// RULE 4: Must not mutate its input  'state' argment
+// RULE 4: MISLEADING >> Must not mutate its input 'state' argment
+//  ----> corner case: object === object works, but object === [1,2,3]      
+// never write state and = together!
+
+// Removing an element from an array 
+//         bad... state.pop()
+//            good... state.filter(element => element !== 'hi')
+
+// Adding an element to an array
+//          bad... state.push('hi')
+//              good... [...state,'hi']
+
+// Replacing an element to an array
+//          bad... state[0]='hi'
+//              good... state.map(el => el === 'hi' ? 'bye' : el)
+
+// Updateing a property in an object
+//          bad... state.name='Sam'
+//              good... {...state, name:'Sam'}
+
+// Adding a property to an object
+//          bad... state.age=30
+//              good... {...state, age:30}
+
+// Removing a property from an object
+//          bad... delete state.name
+//              good... {...state, age:undefined}
+//                          _.omit(state,'age')
+
