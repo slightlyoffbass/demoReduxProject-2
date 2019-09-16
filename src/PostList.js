3302b7ed3274f03ab9b0941f0906047faffa0d53
jeff.smith <slightlyoffbass@gmail.com>
2019-09-16 15:07:29 -0500

    add mapStateToProps

--- src/PostList.js
+++ src/PostList.js
@@ -17,4 +17,11 @@ class PostList extends React.Component{
     }
 }
 
-export default connect(null,{fetchPosts})(PostList);
+const mapStateToProps = (state) =>{
+    return { posts: state.posts};
+}
+
+export default connect(
+    mapStateToProps
+    ,{fetchPosts}
+    )(PostList);
