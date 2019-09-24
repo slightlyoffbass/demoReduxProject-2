4229ca8519d8e19c7967cdf5762b0482ed8bc39f
jeff.smith <slightlyoffbass@gmail.com>
2019-09-24 16:13:53 -0500

    move logic to mapstatetoprops

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -9,7 +9,7 @@ class UserHeader extends React.Component {
     }
 
     render () {
-        const user = this.props.users.find( user => user.id === this.props.userId);
+        const {user} =this.props;
 
         if (!user){
             return null;
@@ -17,11 +17,10 @@ class UserHeader extends React.Component {
 
         return <div className="header">{user.name}</div>
     }
-
- 
 }
-const mapStateToProps = (state) => {
-    return {users: state.users};
+
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find(user => user.id === ownProps.userId)};
 }
 
 export default connect(mapStateToProps, {fetchUser})(UserHeader);
