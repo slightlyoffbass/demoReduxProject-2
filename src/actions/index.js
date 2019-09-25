1eede7d27d1c9e00257ee2d7e3773bf6398d9e95
jeff.smith <slightlyoffbass@gmail.com>
2019-09-25 10:25:12 -0500

    create momoize private function to prevent multiple calls for user id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../api/jsonplaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -11,14 +12,21 @@ export const fetchPosts = () => async dispatch => {
   
 };
 
-export const fetchUser =  id  => async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+export const fetchUser =  id  =>  dispatch => {
+    _fetchUser(id,dispatch);
 
+};
+
+// underscore for private function
+// memoize allows id to be called once, vs makeing call every time
+const _fetchUser = _.memoize(async(id,dispatch) =>{
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     });
-}
+});
 
     //const response = await jsonPlaceholder.get('/posts')
     // Bad Approach!!! Breaks rules of action creator
