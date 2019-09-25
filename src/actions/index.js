c1e4ad7f83bf869b348ddb961daf8774736bcb69
jeff.smith <slightlyoffbass@gmail.com>
2019-09-25 10:29:20 -0500

    add comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,6 +16,7 @@ export const fetchUser =  id  =>  dispatch => _fetchUser(id,dispatch);
 
 // underscore for private function
 // memoize allows id to be called once, vs makeing call every time
+// this solution does not allow you to refetch user, but is a smaller code change
 const _fetchUser = _.memoize(async(id,dispatch) =>{
     const response = await jsonPlaceholder.get(`/users/${id}`);
     
